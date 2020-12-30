7ce5e75c623e571fd403ab1453965903b828dec6
Andress <AndressMartin@users.noreply.github.com>
2020-12-30 14:32:12 -0300

    Movimento em Grid

---
+++ Torrois/Assets/Scripts/playerMoveGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoveGrid : MonoBehaviour
+{
+    public float velocidade = 5f;
+    public Transform pontoMov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pontoMov.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pontoMov.position, velocidade * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, pontoMov.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                pontoMov.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                pontoMov.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
+        }
+    }
+}
