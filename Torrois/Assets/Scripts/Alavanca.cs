5ffc6e23fa291dab07def8a88fedd876186a74a5
TorroisBr <martinshenrique00@gmail.com>
2021-01-01 19:57:16 -0300

    botao e alavanca interagindo com caixas

---
+++ Torrois/Assets/Scripts/Alavanca.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alavanca : MonoBehaviour
+{
+    public Transform pontoMov;
+    public int ativado;
+    playerMoveGrid player = new playerMoveGrid();
+    public BoxCollider2D hitbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ativado = 1;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Alavanca valor: " +ativado);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ativado *= -1;
+    }
+   
+    
+}
