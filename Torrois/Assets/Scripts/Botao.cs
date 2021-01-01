d6c3ec037ddc48225d98a009085740bcf8583c9b
TorroisBr <martinshenrique00@gmail.com>
2021-01-01 20:46:11 -0300

    Alavanca + botao

---
+++ Torrois/Assets/Scripts/Botao.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao : MonoBehaviour
+{
+    public Transform pontoMov;
+    public bool ativado;
+    playerMoveGrid player = new playerMoveGrid();
+    public BoxCollider2D hitbox;
+    public float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Botao valor: " + ativado);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Torre")
+        {
+            ativado = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ativado = false;
+        }
+
+    }
+
+
+}
