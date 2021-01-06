85c4f27c7516bb9527ad8832111d1b45b7521510
Andress <AndressMartin@users.noreply.github.com>
2021-01-06 19:53:51 -0300

    Adicionando terceira porta

--- Torrois/Assets/Scripts/Transitar.cs
+++ Torrois/Assets/Scripts/Transitar.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class Transitar : MonoBehaviour
 {
-    public Transform Porta;
     BoxCollider2D portaBoxColl;
     public bool portaOpen;
+    public bool passou;
     // Start is called before the first frame update
     void Start()
     {
-        Porta = gameObject.transform.parent;
-        portaBoxColl = Porta.GetComponent<BoxCollider2D>();
+        portaBoxColl = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (portaOpen)
+        if (portaOpen && !passou)
         {
-            portaBoxColl.enabled = false;
+            portaBoxColl.enabled = true;
         }
+        else
+            portaBoxColl.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            passou = true;
             CameraMov.podeMover = true;
-            gameObject.SetActive(false);
         }
     }
 }
