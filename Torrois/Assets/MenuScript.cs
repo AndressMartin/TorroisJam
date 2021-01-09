6f4b43e78f8bfd68e53998106afaab5a50c97b13
Andress <AndressMartin@users.noreply.github.com>
2021-01-09 19:37:50 -0300

    Menu Principal

---
+++ Torrois/Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+public class MenuScript : MonoBehaviour
+{
+    FMOD.Studio.EventInstance mouseOver;
+
+    private void Start()
+    {
+        mouseOver = RuntimeManager.CreateInstance("event:/sfx/menu_selection");
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOver.start();
+    }
+}
